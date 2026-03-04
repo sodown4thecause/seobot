a466a0c73f3fc622241e284c0909ec4ef22f00b6
Dev <dev@seobot.com>
2026-03-04 18:57:18 +1100

    fix(sanity): default dataset for preview builds

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,10 +1,11 @@
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2026-01-02'
 
-export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
-)
+const configuredDataset = process.env.NEXT_PUBLIC_SANITY_DATASET
+
+export const dataset = configuredDataset === 'preview'
+  ? 'production'
+  : configuredDataset || 'production'
 
 export const projectId = assertValue(
   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
